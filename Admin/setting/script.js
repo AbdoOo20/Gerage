130829df52f28db485cb8e908241ccc1e5ff0f78
Abdelrahman Emad Eldeen Ragab <87204675+AbdoOo20@users.noreply.github.com>
2024-08-28 02:29:57 +0300

    Unit & Setting

---
+++ Admin/setting/script.js
@@ -0,0 +1,71 @@
+import { updateDoc, db, uploadBytes, ref, getDownloadURL, doc, getDoc } from './../../../Database/firebase-config.js';
+
+const settingForm = document.getElementById('settingForm');
+var docRef;
+
+(function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+})();
+
+
+(async function () {
+    docRef = doc(db, 'Settings', "pMbOKWdq6WtCoOzZ3hA9");
+    const docSnap = await getDoc(docRef);
+    const settingData = docSnap.data();
+    document.getElementById("privacyTerms").value = settingData.privacy;
+    document.getElementById("aboutUs").value = settingData.aboutUs;
+    document.getElementById("contactEmail").value = settingData.contactEmail;
+    document.getElementById("contactNumber").value = settingData.contactNumber;
+    document.getElementById("facebookLink").value = settingData.facebook;
+    document.getElementById("instagramLink").value = settingData.instagram;    
+})();
+
+
+settingForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(settingForm);
+    const privacy = formData.get('privacyTerms');
+    const aboutUs = formData.get('aboutUs');
+    const contactEmail = formData.get('contactEmail');
+    const contactNumber = formData.get('contactNumber');
+    const facebookLink = formData.get('facebookLink');
+    const instagramLink = formData.get('instagramLink');
+    await updateDoc(docRef, {
+        privacy: privacy,
+        aboutUs: aboutUs,
+        contactEmail: contactEmail,
+        contactNumber: contactNumber,
+        facebook: facebookLink,
+        instagram: instagramLink
+    }).then(() => {
+        console.log("1");
+        showAlert("Settings Edited Successfully", "success");
+    }).catch((e) => {
+        console.log("2");
+        showAlert(e.message, "danger");
+    });
+});
+
+function showAlert(message, type) { // type => // danger // success // warning
+    const alertContainer = document.getElementById('alert-container');
+    const alertDiv = document.createElement('div');
+    alertDiv.className = `alert alert-${type}`;
+    alertDiv.role = 'alert';
+    alertDiv.innerText = message;
+    alertContainer.appendChild(alertDiv);
+    setTimeout(() => {
+        alertDiv.remove();
+    }, 3000);
+}
